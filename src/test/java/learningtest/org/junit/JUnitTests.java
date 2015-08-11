fc3258c922b4e30edf805f2c4138facfda3bcdd6
izeye <izeye@naver.com>
2015-08-12 00:40:27 +0900

    Initial commit

---
+++ src/test/java/learningtest/org/junit/JUnitTests.java
@@ -0,0 +1,15 @@
+package learningtest.org.junit;
+
+import org.junit.Test;
+
+/**
+ * Created by izeye on 15. 8. 12..
+ */
+public class JUnitTests {
+	
+	@Test
+	public void test() {
+		System.out.println("Hello, JUnit!");
+	}
+	
+}
