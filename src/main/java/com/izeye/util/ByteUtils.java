a79aef853ad126d8a05df9d32b7999ec27c7fa3c
Johnny Lim <izeye@naver.com>
2016-03-19 16:01:24 +0900

    Add a Cipher test

---
+++ src/main/java/com/izeye/util/ByteUtils.java
@@ -0,0 +1,56 @@
+package com.izeye.util;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by izeye on 16. 3. 19..
+ */
+public abstract class ByteUtils {
+	
+	public static String bytes2HexString(byte[] bytes) {
+		StringBuilder sb = new StringBuilder(bytes.length * 2);
+		for (byte b : bytes) {
+			sb.append(byte2HexString(b));
+		}
+		return sb.toString();
+	}
+	
+	public static String byte2HexString(byte b) {
+		return new StringBuilder(2)
+				.append(byte2HexChar((byte) ((b >>> 4) & 0xF)))
+				.append(byte2HexChar((byte) (b & 0xF))).toString();
+	}
+	
+	public static byte[] hexString2Bytes(String hex) {
+		byte[] bytes = new byte[hex.length() / 2];
+		int byteOffset = 0;
+		for (int i = 0; i < hex.length(); i += 2) {
+			bytes[byteOffset] = (byte) (hexChar2Byte(hex.charAt(i)) << 4
+					| hexChar2Byte(hex.charAt(i + 1)));
+			byteOffset++;
+		}
+		return bytes;
+	}
+	
+	private static final String HEX_CHARS = "0123456789abcdef";
+	
+	private static final Map<Character, Byte> BYTE_BY_HEX_CHAR;
+	static {
+		Map<Character, Byte> byteByHexChar = new HashMap<>(16);
+		for (int i = 0; i < HEX_CHARS.length(); i++) {
+			byteByHexChar.put(HEX_CHARS.charAt(i), (byte) i);
+		}
+		BYTE_BY_HEX_CHAR = Collections.unmodifiableMap(byteByHexChar);
+	}
+	
+	public static char byte2HexChar(byte b) {
+		return HEX_CHARS.charAt(b);
+	}
+	
+	public static byte hexChar2Byte(char hex) {
+		return BYTE_BY_HEX_CHAR.get(hex);
+	}
+	
+}
---
+++ src/test/java/learningtest/javax/crypto/CipherTests.java
@@ -0,0 +1,98 @@
+package learningtest.javax.crypto;
+
+import com.izeye.util.ByteUtils;
+import org.junit.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by izeye on 16. 3. 16..
+ */
+public class CipherTests {
+	
+	@Test
+	public void test() {
+		String keyAlgorithm = "AES";
+//		String cipherAlgorithm = "AES/ECB/PKCS5PADDING";
+//		String cipherAlgorithm = "AES/CBC/PKCS5PADDING";
+		String cipherAlgorithm = "AES/GCM/PKCS5PADDING";
+		int keySize = 128;
+		int authenticationTagLength = 128;
+		
+		String message = "This is a test message.";
+		
+		try {
+			KeyGenerator keyGenerator = KeyGenerator.getInstance(keyAlgorithm);
+			keyGenerator.init(keySize);
+			SecretKey secretKey = keyGenerator.generateKey();
+			byte[] encodedSecretKey = secretKey.getEncoded();
+			System.out.println(encodedSecretKey.length);
+			
+			byte[] iv = new byte[keySize / 8];
+			SecureRandom random = new SecureRandom();
+			random.nextBytes(iv);
+			
+			String hexSecretKey = ByteUtils.bytes2HexString(encodedSecretKey);
+			System.out.println(hexSecretKey);
+			assertThat(ByteUtils.hexString2Bytes(hexSecretKey)).isEqualTo(encodedSecretKey);
+			
+			Cipher encryptor = Cipher.getInstance(cipherAlgorithm);
+
+			// For ECB
+//			encryptor.init(Cipher.ENCRYPT_MODE, secretKey);
+
+			// For CBC
+//			encryptor.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
+
+			// For GCM
+			encryptor.init(Cipher.ENCRYPT_MODE, secretKey,
+					new GCMParameterSpec(authenticationTagLength, iv));
+
+			byte[] encrypted = encryptor.doFinal(message.getBytes());
+			String encoded = Base64.getEncoder().encodeToString(encrypted);
+			System.out.println(encoded);
+			
+			Cipher decryptor = Cipher.getInstance(cipherAlgorithm);
+			
+			// For ECB
+//			decryptor.init(Cipher.DECRYPT_MODE, secretKey);
+			
+			// For CBC
+//			decryptor.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+			
+			// For GCM
+			decryptor.init(Cipher.DECRYPT_MODE, secretKey,
+					new GCMParameterSpec(authenticationTagLength, iv));
+
+			byte[] decoded = Base64.getDecoder().decode(encoded);
+			byte[] decrypted = decryptor.doFinal(decoded);
+			System.out.println(new String(decrypted));
+		} catch (NoSuchAlgorithmException ex) {
+			throw new RuntimeException(ex);
+		} catch (NoSuchPaddingException ex) {
+			throw new RuntimeException(ex);
+		} catch (InvalidKeyException ex) {
+			throw new RuntimeException(ex);
+		} catch (BadPaddingException ex) {
+			throw new RuntimeException(ex);
+		} catch (IllegalBlockSizeException ex) {
+			throw new RuntimeException(ex);
+		} catch (InvalidAlgorithmParameterException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+}
