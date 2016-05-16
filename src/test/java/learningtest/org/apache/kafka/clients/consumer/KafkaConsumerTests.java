6a93bd606bc885d7c0ef675e3805d5232884b7bd
Johnny Lim <izeye@naver.com>
2016-05-16 11:28:01 +0900

    Add Kafka tests

---
+++ src/test/java/learningtest/org/apache/kafka/clients/consumer/KafkaConsumerTests.java
@@ -0,0 +1,42 @@
+package learningtest.org.apache.kafka.clients.consumer;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+
+/**
+ * Created by izeye on 16. 5. 16..
+ */
+public class KafkaConsumerTests {
+	
+	@Test
+	public void test() {
+		Properties properties = new Properties();
+		properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(GROUP_ID_CONFIG, "test");
+		properties.setProperty(ENABLE_AUTO_COMMIT_CONFIG, "true");
+		properties.setProperty(AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+		properties.setProperty(SESSION_TIMEOUT_MS_CONFIG, "30000");
+		properties.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+		Consumer<String, String> consumer = new KafkaConsumer<>(properties);
+		consumer.subscribe(Arrays.asList("my-topic"));
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(100);
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.printf("Offset = %d, key = %s, value = %s%n",
+						record.offset(), record.key(), record.value());
+			}
+		}
+	}
+	
+}
---
+++ src/test/java/learningtest/org/apache/kafka/clients/producer/KafkaProducerTests.java
@@ -0,0 +1,39 @@
+package learningtest.org.apache.kafka.clients.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+/**
+ * Created by izeye on 16. 5. 15..
+ */
+public class KafkaProducerTests {
+	
+	@Test
+	public void test() {
+		Properties properties = new Properties();
+		properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		
+		Producer<String, String> producer = new KafkaProducer<>(properties);
+		for (int i = 0; i < 100; i++) {
+			producer.send(new ProducerRecord<>(
+					"my-topic", Integer.toString(i), Integer.toString(i)));
+		}
+		
+		// NOTE: Should sleep because `send()` is asynchronous.
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+}
