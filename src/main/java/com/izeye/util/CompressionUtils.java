4dfe5f44a67e5c0de4ccb8957fe0469ee3ab8fee
Johnny Lim <izeye@naver.com>
2016-05-31 10:48:59 +0900

    Release native resources of Deflater/Inflater explicitly
    If releasing native resources depends on finalizer, with intensive loads native memory leak can occur.
    
    To release native resources as soon as possible is generally good.

--- src/main/java/com/izeye/util/CompressionUtils.java
+++ src/main/java/com/izeye/util/CompressionUtils.java
@@ -13,31 +13,48 @@ public abstract class CompressionUtils {
 	private static final int BUFFER_SIZE = 1024;
 
 	public static byte[] compress(byte[] original) {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		Deflater compresser = new Deflater();
-		compresser.setInput(original);
-		compresser.finish();
-		byte[] buffer = new byte[BUFFER_SIZE];
-		int written;
-		while ((written = compresser.deflate(buffer)) != 0) {
-			baos.write(buffer, 0, written);
+		Deflater compresser = null;
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			compresser = new Deflater();
+			compresser.setInput(original);
+			compresser.finish();
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int written;
+			while ((written = compresser.deflate(buffer)) != 0) {
+				baos.write(buffer, 0, written);
+			}
+			return baos.toByteArray();
 		}
-		return baos.toByteArray();
+		finally {
+			if (compresser != null) {
+				// NOTE: This should be called explicitly to avoid possible native memory leak.
+				compresser.end();
+			}
+		}
 	}
 
 	public static byte[] uncompress(byte[] compressed) {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		Inflater inflater = new Inflater();
-		inflater.setInput(compressed);
-		byte[] buffer = new byte[BUFFER_SIZE];
-		int read;
+		Inflater decompresser = null;
 		try {
-			while ((read = inflater.inflate(buffer)) != 0) {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			decompresser = new Inflater();
+			decompresser.setInput(compressed);
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int read;
+			while ((read = decompresser.inflate(buffer)) != 0) {
 				baos.write(buffer, 0, read);
 			}
 			return baos.toByteArray();
-		} catch (DataFormatException ex) {
+		}
+		catch (DataFormatException ex) {
 			throw new RuntimeException(ex);
+		}
+		finally {
+			if (decompresser != null) {
+				// NOTE: This should be called explicitly to avoid possible native memory leak.
+				decompresser.end();
+			}
 		}
 	}
 	
