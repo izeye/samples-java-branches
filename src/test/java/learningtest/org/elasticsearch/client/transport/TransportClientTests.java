be2b5992e90c44f251975a123c0026fdf51d7926
Johnny Lim <izeye@naver.com>
2016-07-13 18:11:02 +0900

    Add a TransportClient test

---
+++ src/test/java/learningtest/org/elasticsearch/client/transport/TransportClientTests.java
@@ -0,0 +1,37 @@
+package learningtest.org.elasticsearch.client.transport;
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by izeye on 16. 7. 13..
+ */
+public class TransportClientTests {
+	
+	@Test
+	public void test() throws UnknownHostException {
+		Map<String, String> map = new HashMap<>();
+		map.put("firstName", "Johnny");
+		map.put("lastName", "Lim");
+		
+		TransportClient client = TransportClient.builder().settings(settings()).build();
+		client.addTransportAddress(
+				new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+		IndexResponse indexResponse = client.prepareIndex("logstash", "logstash").setSource(map).get();
+		System.out.println(indexResponse);
+	}
+
+	private Settings settings() {
+		return Settings.settingsBuilder()
+				.put("cluster.name", "elasticsearch").build();
+	}
+
+}
