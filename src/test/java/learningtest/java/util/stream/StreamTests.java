0e1696faec4dcbfa0628737bd544fe97d46aa853
Johnny Lim <izeye@naver.com>
2016-09-20 15:08:09 +0900

    Align with the original intention of the page 30 of http://www.slideshare.net/mariofusco/monadic-java

--- src/test/java/learningtest/java/util/stream/StreamTests.java
+++ src/test/java/learningtest/java/util/stream/StreamTests.java
@@ -25,12 +25,13 @@ public class StreamTests {
 	public void test() {
 		int n = 10;
 		List<int[]> pairs = Stream.iterate(1, i -> i + 1).limit(n)
-				.flatMap(i -> Stream.iterate(1, j -> j + 1).limit(n).map(j -> new int[]{i, j}))
+				.flatMap(i -> Stream.iterate(1, j -> j + 1).limit(i).map(j -> new int[]{i, j}))
 				.filter(pair -> isPrime(pair[0] + pair[1]))
 				.collect(toList());
 		pairs.stream().forEach(pair -> System.out.println(Arrays.toString(pair)));
 		assertThat(pairs).doesNotContain(new int[] {1, 1});
-		assertThat(pairs).contains(new int[] {1, 2});
+		assertThat(pairs).doesNotContain(new int[] {1, 2});
+		assertThat(pairs).contains(new int[] {2, 1});
 	}
 
 	private boolean isPrime(int n) {
