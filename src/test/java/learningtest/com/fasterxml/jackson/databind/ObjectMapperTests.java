e86be8c2c4eaa1152917a6c707da01a4fdb8cc6d
Johnny Lim <izeye@naver.com>
2016-12-05 11:45:03 +0900

    Add a test for @JsonIgnore on setters

--- src/test/java/learningtest/com/fasterxml/jackson/databind/ObjectMapperTests.java
+++ src/test/java/learningtest/com/fasterxml/jackson/databind/ObjectMapperTests.java
@@ -1,7 +1,10 @@
 package learningtest.com.fasterxml.jackson.databind;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import lombok.Data;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -49,6 +52,28 @@ public class ObjectMapperTests {
 		assertThat(environment.getName()).isEqualTo("Johnny");
 	}
 
+	@Test
+	public void testJsonIgnoreOnSetter() throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+
+		String json = "{\"firstName\": \"Johnny\", \"lastName\": \"Lim\"}";
+		Person person = mapper.readValue(json, Person.class);
+		System.out.println(person);
+		assertThat(person.getFirstName()).isEqualTo("Johnny");
+		assertThat(person.getLastName()).isNull();
+
+		Person anotherPerson = new Person();
+		anotherPerson.setFirstName("Johnny");
+		anotherPerson.setLastName("Lim");
+		System.out.println(anotherPerson);
+		assertThat(anotherPerson.getFirstName()).isEqualTo("Johnny with @JsonIgnore");
+		assertThat(anotherPerson.getLastName()).isEqualTo("Lim with @JsonIgnore");
+		String anotherJson = mapper.writeValueAsString(anotherPerson);
+		System.out.println(anotherJson);
+		assertThat(anotherJson).isEqualTo("{\"firstName\":\"Johnny with @JsonIgnore\"}");
+
+	}
+
 	static class Foo {
 		private final String barBar;
 
@@ -59,6 +84,25 @@ public class ObjectMapperTests {
 		public String getBarBar() {
 			return barBar;
 		}
+	}
+
+	@Data
+	static class Person {
+
+		@JsonProperty
+		private String firstName;
+		private String lastName;
+
+		@JsonIgnore
+		public void setFirstName(String firstName) {
+			this.firstName = firstName + " with @JsonIgnore";
+		}
+
+		@JsonIgnore
+		public void setLastName(String lastName) {
+			this.lastName = lastName + " with @JsonIgnore";
+		}
+
 	}
 	
 }
