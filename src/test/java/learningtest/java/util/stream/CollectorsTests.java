7c10670837ba2b52ac49ad9ddc63b24af2d57fb4
Johnny Lim <izeye@naver.com>
2016-12-28 10:07:37 +0900

    Add tests for Collectors

---
+++ src/test/java/learningtest/java/util/stream/CollectorsTests.java
@@ -0,0 +1,25 @@
+package learningtest.java.util.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link Collectors}.
+ *
+ * @author Johnny Lim
+ */
+public class CollectorsTests {
+
+	@Test
+	public void testJoining() {
+		List<String> fruits = Arrays.asList("apple", "banana");
+		String joined = fruits.stream().collect(Collectors.joining(", ", "[", "]"));
+		assertThat(joined).isEqualTo("[apple, banana]");
+	}
+
+}
