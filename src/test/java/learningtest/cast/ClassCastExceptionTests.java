ce0b190432710e1f2db3ca2bc89032f59dd0a3b6
Johnny Lim <izeye@naver.com>
2017-02-03 17:21:06 +0900

    Add tests for ClassCastException

---
+++ src/test/java/learningtest/cast/ClassCastExceptionTests.java
@@ -0,0 +1,36 @@
+package learningtest.cast;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Tests for {@link ClassCastException}.
+ *
+ * @author Johnny Lim
+ */
+public class ClassCastExceptionTests {
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void test() {
+		this.thrown.expect(ClassCastException.class);
+		handle(new A());
+	}
+
+	private void handle(IA ia) {
+		System.out.println((B) ia);
+	}
+
+	private interface IA {
+	}
+
+	private static class A implements IA {
+	}
+
+	private static class B extends A {
+	}
+
+}
