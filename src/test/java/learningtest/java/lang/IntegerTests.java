5dd5f8294f8847e1546155840381ade840e53bda
Johnny Lim <izeye@naver.com>
2017-03-10 10:52:51 +0900

    Add a test showing that a return statement for int having a ternary operator with null could be compiled

--- src/test/java/learningtest/java/lang/IntegerTests.java
+++ src/test/java/learningtest/java/lang/IntegerTests.java
@@ -1,6 +1,8 @@
 package learningtest.java.lang;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class IntegerTests {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
 	@Test
 	public void testToHexString() {
 		byte b = 0;
@@ -20,6 +25,16 @@ public class IntegerTests {
 		b = (byte) 0xff;
 		assertThat(Integer.toHexString(b)).isEqualTo("ffffffff");
 		assertThat(Integer.toHexString(b & 0xff)).isEqualTo("ff");
+	}
+
+	@Test
+	public void testTernaryOperatorReturnsNullForInt() {
+		this.thrown.expect(NullPointerException.class);
+		getInt(false);
+	}
+
+	private int getInt(boolean b) {
+		return b ? 1 : null;
 	}
 
 }
