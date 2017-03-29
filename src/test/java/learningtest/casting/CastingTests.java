53d7bb4de09c3cde7348c509034038d87f26bc3b
Johnny Lim <izeye@naver.com>
2017-03-29 15:51:03 +0900

    Add a test for multiple type casting

---
+++ src/test/java/learningtest/casting/CastingTests.java
@@ -0,0 +1,45 @@
+package learningtest.casting;
+
+import org.junit.Test;
+
+/**
+ * Tests for type casting.
+ *
+ * @author Johnny Lim
+ */
+public class CastingTests {
+
+    @Test
+    public void testMultipleTypeCasting() {
+        Object object = new C();
+
+        // When is this useful?
+        ((A & B) object).a();
+        ((A & B) object).b();
+    }
+
+    interface A {
+
+        default void a() {
+            System.out.println("a");
+        }
+
+    }
+
+    interface B {
+
+        default void b() {
+            System.out.println("b");
+        }
+
+    }
+
+    static class C implements A, B {
+
+        public void c() {
+            System.out.println("c");
+        }
+
+    }
+
+}
