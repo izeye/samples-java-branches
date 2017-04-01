4bdb2bff3892a2cef9b6df4c6b0d9efe26dfb917
Johnny Lim <izeye@naver.com>
2017-04-01 09:17:39 +0900

    Update OmitStackTraceInFastThrowTests

--- src/test/java/learningtest/exception/OmitStackTraceInFastThrowTests.java
+++ src/test/java/learningtest/exception/OmitStackTraceInFastThrowTests.java
@@ -12,25 +12,23 @@ import org.junit.Test;
 public class OmitStackTraceInFastThrowTests {
 
     @Test
-    public void test() {
+    public void testReferenceNull() {
         for (int i = 0; i < 1_000_000; i++) {
             try {
-                ((Object) null).getClass();
+                referenceNull();
 
                 // This is not triggering it. What is triggering the omission?
 //                throw new NullPointerException();
             }
             catch (Exception ex) {
                 if (ex.getStackTrace().length == 0) {
-                    // "Stack trace has been omitted at 115714" was printed in my PC.
                     System.out.println("Stack trace has been omitted at " + i);
                     ex.printStackTrace(System.out);
 
                     try {
-                        ((Object) null).getClass();
+                        referenceNull();
                     }
                     catch (Exception nested) {
-                        // This is not affected. What is the same stack trace for omission?
                         System.out.println(nested.getStackTrace().length);
                         nested.printStackTrace(System.out);
                     }
@@ -38,6 +36,42 @@ public class OmitStackTraceInFastThrowTests {
                 }
             }
         }
+    }
+
+    @Test
+    public void testReferenceNullIndirectly() {
+        for (int i = 0; i < 1_000_000; i++) {
+            try {
+                invokeReferenceNullIndirectly();
+            }
+            catch (Exception ex) {
+                if (ex.getStackTrace().length == 0) {
+                    System.out.println("Stack trace has been omitted at " + i);
+                    ex.printStackTrace(System.out);
+
+                    try {
+                        invokeReferenceNullIndirectlyAnother();
+                    }
+                    catch (Exception nested) {
+                        System.out.println(nested.getStackTrace().length);
+                        nested.printStackTrace(System.out);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    private void referenceNull() {
+        ((Object) null).getClass();
+    }
+
+    private void invokeReferenceNullIndirectly() {
+        referenceNull();
+    }
+
+    private void invokeReferenceNullIndirectlyAnother() {
+        referenceNull();
     }
 
 }
