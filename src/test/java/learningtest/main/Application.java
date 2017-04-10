258f4eca3a39e9b321c8637a0888c9a6e73a15ff
Johnny Lim <izeye@naver.com>
2017-04-10 10:24:42 +0900

    Add an empty main() application for testing "-verbose:class": http://izeye.blogspot.kr/2017/04/get-locations-for-loaded-classes-in-java.html

---
+++ src/test/java/learningtest/main/Application.java
@@ -0,0 +1,13 @@
+package learningtest.main;
+
+/**
+ * Empty {@code main()} application.
+ *
+ * @author Johnny Lim
+ */
+public class Application {
+
+    public static void main(String[] args) {
+    }
+
+}
