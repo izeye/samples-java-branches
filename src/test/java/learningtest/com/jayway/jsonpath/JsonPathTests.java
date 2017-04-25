a5bef9fadf2654570f9c5b3a8225a900c309f3b3
Johnny Lim <izeye@naver.com>
2017-04-25 17:04:11 +0900

    Add tests for JsonPath

--- src/test/java/learningtest/com/jayway/jsonpath/JsonPathTests.java
+++ src/test/java/learningtest/com/jayway/jsonpath/JsonPathTests.java
@@ -1,7 +1,12 @@
 package learningtest.com.jayway.jsonpath;
 
+import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.PathNotFoundException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,11 +17,37 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class JsonPathTests {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	private final String json = "{\"a1\":{\"b1\":{\"c1\":\"a1b1c1\"},\"b2\":[{\"c1\":\"a1b2c1\"},{\"c2\":\"a1b2c2\"},{\"c3\":\"a1b2c3\"}]}}";
+
 	@Test
 	public void test() {
-		String json = "{\"a1\":{\"b1\":{\"c1\":\"a1b1c1\"},\"b2\":[{\"c1\":\"a1b2c1\"},{\"c2\":\"a1b2c2\"},{\"c3\":\"a1b2c3\"}]}}";
-		String a1b1c1 = JsonPath.parse(json).read("a1.b1.c1", String.class);
+		String a1b1c1 = JsonPath.parse(this.json).read("a1.b1.c1", String.class);
 		assertThat(a1b1c1).isEqualTo("a1b1c1");
+	}
+
+	@Test
+	public void testMissingLeaf() {
+		this.thrown.expect(PathNotFoundException.class);
+		JsonPath.parse(this.json).read("a1.b1.c2", String.class);
+	}
+
+	@Test
+	public void testMissingLeafWithDefaultPathLeafToNullOption() {
+		Configuration conf = Configuration.defaultConfiguration().addOptions(
+				Option.DEFAULT_PATH_LEAF_TO_NULL);
+		String a1b1c2 = JsonPath.parse(this.json, conf).read("a1.b1.c2", String.class);
+		assertThat(a1b1c2).isNull();
+	}
+
+	@Test
+	public void testMissingIntermediate() {
+		this.thrown.expect(PathNotFoundException.class);
+		Configuration conf = Configuration.defaultConfiguration().addOptions(
+				Option.DEFAULT_PATH_LEAF_TO_NULL);
+		JsonPath.parse(this.json, conf).read("a2.b1.c1", String.class);
 	}
 
 }
