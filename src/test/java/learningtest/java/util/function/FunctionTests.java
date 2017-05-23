b468659c5921f338e5be24922c41a0099b6025a3
Johnny Lim <izeye@naver.com>
2017-05-23 14:07:37 +0900

    Add tests for Function from https://dzone.com/articles/java-lambda-expressions-vs

---
+++ src/test/java/learningtest/java/util/function/FunctionTests.java
@@ -0,0 +1,42 @@
+package learningtest.java.util.function;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link Function}.
+ *
+ * @author Johnny Lim
+ */
+public class FunctionTests {
+
+	@Test
+	public void testLambda() {
+		List<String> digits = Arrays.asList("1", "2", "3", "4", "5");
+		List<Integer> numbers = map(s -> new Integer(s), digits);
+		assertThat(numbers).containsExactly(1, 2, 3, 4, 5);
+	}
+
+	@Test
+	public void testMethodReference() {
+		List<String> digits = Arrays.asList("1", "2", "3", "4", "5");
+		List<Integer> numbers = map(Integer::new, digits);
+		assertThat(numbers).containsExactly(1, 2, 3, 4, 5);
+	}
+
+	private static <T, R> List<R> map(Function<T, R> function, List<T> source) {
+		List<R> results = new ArrayList<>();
+		for (T item : source) {
+			R result = function.apply(item);
+			results.add(result);
+		}
+		return results;
+	}
+
+}
