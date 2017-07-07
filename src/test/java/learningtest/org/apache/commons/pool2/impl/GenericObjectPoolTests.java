275f895369b06d713dced7bac9c84a34fe1fa5bb
Johnny Lim <izeye@naver.com>
2017-07-07 20:44:48 +0900

    Add more tests for GenericObjectPool

--- src/test/java/learningtest/org/apache/commons/pool2/impl/GenericObjectPoolTests.java
+++ src/test/java/learningtest/org/apache/commons/pool2/impl/GenericObjectPoolTests.java
@@ -8,6 +8,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Tests for {@link GenericObjectPool}.
  */
@@ -26,12 +28,46 @@ public class GenericObjectPoolTests {
 		objectPool.returnObject(new SomeObject());
 	}
 
+	@Test
+	public void testFailedToCreateAndReturnObjectWithNull() {
+		GenericObjectPool<SomeObject> objectPool = new GenericObjectPool<>(
+				new FailingPooledSomeObjectFactory());
+
+		SomeObject borrowed = null;
+		try {
+			borrowed = objectPool.borrowObject();
+		}
+		catch (Exception ex) {
+			assertThat(ex).isInstanceOf(RuntimeException.class);
+			assertThat(ex.getMessage()).isEqualTo("Failed to create SomeObject.");
+		}
+		assertThat(borrowed).isNull();
+
+		this.thrown.expect(IllegalStateException.class);
+		this.thrown.expectMessage("Returned object not currently part of this pool");
+		objectPool.returnObject(null);
+	}
+
 	private static class PooledSomeObjectFactory
 			extends BasePooledObjectFactory<SomeObject> {
 
 		@Override
 		public SomeObject create() throws Exception {
 			return new SomeObject();
+		}
+
+		@Override
+		public PooledObject<SomeObject> wrap(SomeObject obj) {
+			return new DefaultPooledObject<>(obj);
+		}
+	}
+
+	private static class FailingPooledSomeObjectFactory
+			extends BasePooledObjectFactory<SomeObject> {
+
+		@Override
+		public SomeObject create() throws Exception {
+			throw new RuntimeException("Failed to create SomeObject.");
 		}
 
 		@Override
