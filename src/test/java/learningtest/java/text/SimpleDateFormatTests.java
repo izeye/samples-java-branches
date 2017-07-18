644aa9b1a5ae4eb34a2aa0a2f593e4c3e2828863
Johnny Lim <izeye@naver.com>
2017-07-18 19:07:19 +0900

    Add a test for SimpleDateFormat

--- src/test/java/learningtest/java/text/SimpleDateFormatTests.java
+++ src/test/java/learningtest/java/text/SimpleDateFormatTests.java
@@ -1,9 +1,12 @@
 package learningtest.java.text;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tests for {@link SimpleDateFormat}.
@@ -19,6 +22,16 @@ public class SimpleDateFormatTests {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 		String formatted = format.format(date);
 		System.out.println(formatted);
+	}
+
+	@Test
+	public void testParseWithWrongFormat() throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+		Date parsed = formatter.parse("2017-07-18");
+		System.out.println(parsed);
+
+		// NOTE: Expected 'ParseException' but just returns a wrong value.
+		assertThat(parsed).isEqualTo(formatter.parse("20161207"));
 	}
 
 }
