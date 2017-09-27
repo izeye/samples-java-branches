fa80902b6ffef108f6ef65adf1ea9dd977f0f318
Johnny Lim <izeye@naver.com>
2017-09-27 11:41:29 +0900

    Add a test for Stream.map() with List

--- src/test/java/learningtest/java/util/ListTests.java
+++ src/test/java/learningtest/java/util/ListTests.java
@@ -2,10 +2,13 @@ package learningtest.java.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tests for {@link List}.
@@ -25,6 +28,20 @@ public class ListTests {
 
 		this.thrown.expect(IndexOutOfBoundsException.class);
 		numbers.subList(0, 10);
+	}
+
+	@Test
+	public void testStreamMap() {
+		List<Number> numbers = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(Long.MAX_VALUE);
+
+		List<Long> longs = numbers.stream().map(number -> number.longValue())
+				.collect(Collectors.toList());
+
+		assertThat(longs).hasSize(2);
+		assertThat(longs.get(0)).isInstanceOf(Long.class);
+		assertThat(longs.get(1)).isInstanceOf(Long.class);
 	}
 
 }
