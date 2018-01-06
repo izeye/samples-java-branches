7f56d52991f2fe295b9e69dad384ac58b58ea074
Johnny Lim <izeye@naver.com>
2018-01-06 10:57:35 +0900

    Add a test for WebDriver

---
+++ src/test/java/learningtest/org/openqa/selenium/WebDriverTests.java
@@ -0,0 +1,40 @@
+package learningtest.org.openqa.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link WebDriver}.
+ *
+ * NOTE: Generally you need to install ChromeDriver first
+ * although this test uses a self-contained executable.
+ * See https://sites.google.com/a/chromium.org/chromedriver/downloads
+ *
+ * @author Johnny Lim
+ */
+public class WebDriverTests {
+
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "./bin/chrome_driver/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+
+		WebElement element = driver.findElement(By.name("q"));
+
+		String query = "Cheese!";
+
+		element.sendKeys(query);
+		element.submit();
+
+		assertThat(driver.getTitle()).contains(query);
+	}
+
+}
