7cd4d01ede37a1c9e6ee0840c1dca8f2a3202c10
Johnny Lim <izeye@naver.com>
2018-02-27 11:36:15 +0900

    Add tests for generics callee parameter type

---
+++ src/test/java/learningtest/generics/GenericsCalleeParameterTypeTests.java
@@ -0,0 +1,34 @@
+package learningtest.generics;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for generics callee parameter type.
+ *
+ * @author Johnny Lim
+ */
+public class GenericsCalleeParameterTypeTests {
+
+	private final Object[] objects = {1, "test"};
+
+	@Test
+	public void test() {
+		testFirst(get(0));
+		testSecond(get(1));
+	}
+
+	private void testFirst(Integer value) {
+		assertThat(value).isEqualTo(1);
+	}
+
+	private void testSecond(String value) {
+		assertThat(value).isEqualTo("test");
+	}
+
+	private <T> T get(int index) {
+		return (T) this.objects[index];
+	}
+
+}
