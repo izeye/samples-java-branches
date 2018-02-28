daedc5eade31cd8c35ca41014849266ac211fd47
Johnny Lim <izeye@naver.com>
2018-02-28 22:44:39 +0900

    Update tests based on @bodewig's advice
    See https://github.com/xmlunit/xmlunit/issues/111#issuecomment-369209178

--- src/test/java/learningtest/org/xmlunit/diff/DiffTests.java
+++ src/test/java/learningtest/org/xmlunit/diff/DiffTests.java
@@ -1,6 +1,5 @@
 package learningtest.org.xmlunit.diff;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.builder.Input;
@@ -16,6 +15,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.xmlunit.diff.ElementSelectors.byNameAndText;
+import static org.xmlunit.diff.ElementSelectors.byXPath;
+import static org.xmlunit.diff.ElementSelectors.conditionalBuilder;
 
 public class DiffTests {
 
@@ -84,13 +86,29 @@ public class DiffTests {
 		assertThat(comparisonResults).containsExactly(ComparisonResult.DIFFERENT);
 	}
 
-	// FIXME: See https://github.com/xmlunit/xmlunit/issues/111#issuecomment-368859900
-	// This doesn't work.
-	@Ignore
 	@Test
 	public void getDifferencesWhenDifferentlyOrderedCollectionShouldHaveNoDifferenceWithGlobalUnordered() {
 		String xml1 = "<persons><person><id>1</id><name>Johnny</name></person><person><id>2</id><name>John</name></person></persons>";
 		String xml2 = "<persons><person><id>2</id><name>John</name></person><person><id>1</id><name>Johnny</name></person></persons>";
+
+		Diff diff = DiffBuilder.compare(Input.fromString(xml1))
+				.withTest(Input.fromString(xml2))
+				.withNodeMatcher(new DefaultNodeMatcher(conditionalBuilder()
+						.whenElementIsNamed("person").thenUse(byXPath("./id", byNameAndText))
+						.elseUse(byNameAndText).build()))
+				.withDifferenceEvaluator(
+						DifferenceEvaluators.chain(
+								DifferenceEvaluators.Default,
+								DifferenceEvaluators.downgradeDifferencesToSimilar(ComparisonType.CHILD_NODELIST_SEQUENCE)))
+				.checkForSimilar()
+				.build();
+		assertThat(diff.hasDifferences()).isFalse();
+	}
+
+	@Test
+	public void getDifferencesWhenDifferentlyOrderedCollectionShouldHaveNoDifferenceWithGlobalUnordered2() {
+		String xml1 = "<persons><person>Johnny</person><person>John</person></persons>";
+		String xml2 = "<persons><person>John</person><person>Johnny</person></persons>";
 
 		Diff diff = DiffBuilder.compare(Input.fromString(xml1))
 				.withTest(Input.fromString(xml2))
