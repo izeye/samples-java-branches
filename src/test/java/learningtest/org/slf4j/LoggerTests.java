8c903e8484c90070263ffbd8359ff7646b17b123
Johnny Lim <izeye@naver.com>
2018-04-23 11:20:35 +0900

    Add a test for Logger with an extra last Throwable parameter

--- src/test/java/learningtest/org/slf4j/LoggerTests.java
+++ src/test/java/learningtest/org/slf4j/LoggerTests.java
@@ -5,23 +5,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Created by izeye on 15. 8. 26..
+ * Tests for {@link Logger}.
+ *
+ * @author Johnny Lim
  */
 public class LoggerTests {
-  
-  Logger logger = LoggerFactory.getLogger(getClass());
-  
-  @Test
-  public void testParameterizedMessage() {
-    logger.debug("This is a test.");
-    
-    // Will use `void debug(String format, Object arg);`.
-    logger.debug("My name is {}", "Johnny");
 
-    RuntimeException ex = new RuntimeException("Test exception");
-    // Will use `void debug(String msg, Throwable t);`.
-    // So `{}` doesn't work as expected.
-    logger.debug("Unexpected exception: {}", ex);
-  }
-  
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Test
+	public void testParameterizedMessage() {
+		this.logger.debug("This is a test.");
+
+		// Will use `void debug(String format, Object arg);`.
+		this.logger.debug("My name is {}.", "Johnny");
+
+		RuntimeException ex = new RuntimeException("Test exception");
+		// Will use `void debug(String msg, Throwable t);`.
+		// So `{}` doesn't work as expected.
+		this.logger.debug("Unexpected exception: {}", ex);
+	}
+
+	@Test
+	public void testExtraLastThrowableParameter() {
+		RuntimeException ex = new RuntimeException("Test exception");
+		this.logger.error("My name is {}.", "Johnny", ex);
+	}
+
 }
