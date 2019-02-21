d62c0730ac7456023a5b489a0e0d5c51534fb43e
Johnny Lim <izeye@naver.com>
2019-02-21 12:12:10 +0900

    Add more tests for Iterator

--- src/test/java/learningtest/java/util/IteratorTests.java
+++ src/test/java/learningtest/java/util/IteratorTests.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 /**
  * Tests for {@link Iterator}.
@@ -24,6 +26,30 @@ public class IteratorTests {
 		iterator.forEachRemaining(names::add);
 
 		assertThat(names).containsExactly("Johnny", "John");
+	}
+
+	@Test
+	public void nextWhenRemovingWithIteratorShouldWork() {
+		List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+		Iterator<Integer> iterator = list.iterator();
+		assertThat(iterator.next()).isEqualTo(1);
+
+		iterator.remove();
+		assertThat(list).hasSize(3);
+		assertThat(iterator.next()).isEqualTo(2);
+	}
+
+	@Test
+	public void nextWhenRemovingWithListWillThrowConcurrentModificationException() {
+		List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+		Iterator<Integer> iterator = list.iterator();
+		Integer element = iterator.next();
+		assertThat(element).isEqualTo(1);
+
+		list.remove(element);
+		assertThat(list).hasSize(3);
+		assertThatExceptionOfType(ConcurrentModificationException.class)
+				.isThrownBy(() -> iterator.next());
 	}
 
 }
