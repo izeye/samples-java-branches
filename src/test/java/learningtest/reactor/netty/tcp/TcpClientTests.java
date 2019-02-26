18017b1d6b6debc496e0409425498a81eef0ad10
Johnny Lim <izeye@naver.com>
2019-02-26 23:56:40 +0900

    Handle reconnect for TcpClient

--- src/test/java/learningtest/reactor/netty/tcp/TcpClientTests.java
+++ src/test/java/learningtest/reactor/netty/tcp/TcpClientTests.java
@@ -16,23 +16,32 @@ import java.util.concurrent.TimeUnit;
  */
 public class TcpClientTests {
 
+	private TcpClient tcpClient;
+
 	// Run ServerSocketTests.test() first.
 	@Test
 	@Ignore
 	public void test() {
 		Flux<String> flux = Flux.interval(Duration.ofSeconds(1)).map(i -> i + "\n");
 
-		TcpClient.create()
+		this.tcpClient = TcpClient.create()
 				.host("localhost")
 				.port(8080)
 				.handle((in, out) -> out
 						.options(NettyPipeline.SendOptions::flushOnEach)
 						.sendString(flux)
 						.neverComplete())
-				.connect()
-				.subscribe();
+				.doOnDisconnected((connection) -> connectAndSubscribe());
+		connectAndSubscribe();
 
 		sleep(60);
+	}
+
+	private void connectAndSubscribe() {
+		this.tcpClient
+				.connect()
+				.retryBackoff(Long.MAX_VALUE, Duration.ofSeconds(1), Duration.ofMinutes(1))
+				.subscribe();
 	}
 
 	private void sleep(int seconds) {
