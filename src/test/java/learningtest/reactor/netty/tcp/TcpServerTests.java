8dc308cb880d3d4aa1ec890c9f071893893de5a4
Johnny Lim <izeye@naver.com>
2019-02-28 07:39:58 +0900

    Add TcpClient interacting with TcpServer

--- src/test/java/learningtest/reactor/netty/tcp/TcpServerTests.java
+++ src/test/java/learningtest/reactor/netty/tcp/TcpServerTests.java
@@ -1,9 +1,14 @@
 package learningtest.reactor.netty.tcp;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.netty.NettyPipeline;
+import reactor.netty.tcp.TcpClient;
 import reactor.netty.tcp.TcpServer;
 
 /**
@@ -16,9 +21,11 @@ public class TcpServerTests {
 	@Ignore
 	@Test
 	public void test() {
+		AtomicReference<Integer> portReference = new AtomicReference<>();
 		TcpServer.create()
 				.doOnConnection(
 						(connection) -> System.out.println("Connected: " + connection))
+				.doOnBound((server) -> portReference.set(server.port()))
 				.handle((in, out) ->
 						in.receive()
 								.asString()
@@ -27,6 +34,16 @@ public class TcpServerTests {
 								.then())
 				.bindNow();
 
+		Flux<String> flux = Flux.interval(Duration.ofSeconds(1)).map(i -> i + "\n");
+
+		TcpClient.create()
+				.port(portReference.get())
+				.handle((in, out) ->
+						out.options(NettyPipeline.SendOptions::flushOnEach)
+								.sendString(flux)
+								.neverComplete())
+				.connect()
+				.subscribe();
 		sleep(30);
 	}
 
