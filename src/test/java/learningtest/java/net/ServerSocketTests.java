f218e504bd938ce2c92978b124093cd741383662
Johnny Lim <izeye@naver.com>
2019-03-01 08:45:19 +0900

    Make server handle multiple clients

--- src/test/java/learningtest/java/net/ServerSocketTests.java
+++ src/test/java/learningtest/java/net/ServerSocketTests.java
@@ -21,15 +21,27 @@ public class ServerSocketTests {
 	@Ignore
 	public void test() throws IOException {
 		ServerSocket serverSocket = new ServerSocket(8080);
-		Socket socket = serverSocket.accept();
-		InputStream is = socket.getInputStream();
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		String line;
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
+		System.out.println("Listen: " + serverSocket.getLocalPort());
+
+		while (true) {
+			Socket socket = serverSocket.accept();
+			System.out.println("Connected: " + socket);
+
+			new Thread(() -> handleClient(socket)).start();
 		}
-		br.close();
-		socket.close();
+	}
+
+	private void handleClient(Socket socket) {
+		try (InputStream is = socket.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		}
+		catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 
 }
