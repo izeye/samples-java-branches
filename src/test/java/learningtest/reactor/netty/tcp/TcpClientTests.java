c6334e64cd06882b24c7bf8ab8d86f1cdfdb8142
Johnny Lim <izeye@naver.com>
2019-03-05 12:41:31 +0900

    Add a test for reconnection failure in TcpClient with UnicastProcessor
    See https://github.com/micrometer-metrics/micrometer/pull/1251#issuecomment-469220750

--- src/test/java/learningtest/reactor/netty/tcp/TcpClientTests.java
+++ src/test/java/learningtest/reactor/netty/tcp/TcpClientTests.java
@@ -3,8 +3,10 @@ package learningtest.reactor.netty.tcp;
 import org.junit.Ignore;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
 import reactor.netty.NettyPipeline;
 import reactor.netty.tcp.TcpClient;
+import reactor.util.concurrent.Queues;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -20,7 +22,7 @@ public class TcpClientTests {
 	// Run ServerSocketTests.test() first.
 	@Test
 	@Ignore
-	public void test() {
+	public void testWithFlux() {
 		Flux<String> flux = Flux.interval(Duration.ofSeconds(1)).map(i -> i + "\n");
 
 		AtomicReference<TcpClient> tcpClientReference = new AtomicReference<>();
@@ -32,6 +34,34 @@ public class TcpClientTests {
 						.sendString(flux)
 						.neverComplete())
 				.doOnDisconnected((connection) -> connectAndSubscribe(tcpClientReference.get()));
+		tcpClientReference.set(tcpClient);
+		connectAndSubscribe(tcpClient);
+
+		sleep(60);
+	}
+
+	// Run ServerSocketTests.test() first.
+	@Test
+	@Ignore
+	public void testWithUnicastProcessor() {
+		UnicastProcessor<String> processor = UnicastProcessor.create(Queues.<String>unboundedMultiproducer().get());
+		Flux.interval(Duration.ofSeconds(1)).map(i -> i + "\n").subscribe((m) -> processor.onNext(m));
+		Flux<String> flux = Flux.from(processor);
+
+		AtomicReference<TcpClient> tcpClientReference = new AtomicReference<>();
+		TcpClient tcpClient = TcpClient.create()
+				.host("localhost")
+				.port(8080)
+				.handle((in, out) -> out
+						.options(NettyPipeline.SendOptions::flushOnEach)
+						.sendString(flux)
+						.neverComplete())
+				.doOnDisconnected((connection) -> {
+					// The following exception will be thrown when trying to reconnect:
+					// java.lang.IllegalStateException: UnicastProcessor allows only a single Subscriber
+					System.out.println("doOnDisconnected: " + connection);
+					connectAndSubscribe(tcpClientReference.get());
+				});
 		tcpClientReference.set(tcpClient);
 		connectAndSubscribe(tcpClient);
 
