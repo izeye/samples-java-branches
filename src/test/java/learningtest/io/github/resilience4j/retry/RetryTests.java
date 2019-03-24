104a1e36de75a7fe2290f467858a4e15a6b2aac5
Johnny Lim <izeye@naver.com>
2019-03-24 11:34:55 +0900

    Add a test for Resilience4j retry

---
+++ src/test/java/learningtest/io/github/resilience4j/retry/RetryTests.java
@@ -0,0 +1,40 @@
+package learningtest.io.github.resilience4j.retry;
+
+import io.github.resilience4j.retry.Retry;
+import io.vavr.CheckedFunction0;
+import io.vavr.control.Try;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+
+import javax.xml.ws.WebServiceException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+public class RetryTests {
+
+	@Test
+	public void test() {
+		HelloWorldService helloWorldService = mock(HelloWorldService.class);
+		given(helloWorldService.sayHelloWorld()).willThrow(new WebServiceException("BAM!"));
+
+		Retry retry = Retry.ofDefaults("id");
+		CheckedFunction0<String> retryableSupplier = Retry
+				.decorateCheckedSupplier(retry, helloWorldService::sayHelloWorld);
+
+		Try<String> result = Try.of(retryableSupplier)
+				.recover((throwable) -> "Hello world from recovery function");
+
+		BDDMockito.then(helloWorldService).should(times(3)).sayHelloWorld();
+		assertThat(result.get()).isEqualTo("Hello world from recovery function");
+	}
+
+	private interface HelloWorldService {
+
+		String sayHelloWorld();
+
+	}
+
+}
