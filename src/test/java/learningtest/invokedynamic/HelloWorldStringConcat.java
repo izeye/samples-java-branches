39d9cdd4b15b0c6f0692efea9ee2a11af5a92811
Johnny Lim <izeye@naver.com>
2019-04-03 21:50:47 +0900

    Add a demo for invokedynamic for String concatenation in Java 9+
    See https://dzone.com/articles/jdk-9jep-280-string-concatenations-will-never-be-t

---
+++ src/test/java/learningtest/invokedynamic/HelloWorldStringConcat.java
@@ -0,0 +1,14 @@
+package learningtest.invokedynamic;
+
+/**
+ * Demo class for {@code invokedynamic} for {@code String} concatenation in Java 9+.
+ *
+ * @author Johnny Lim
+ */
+public class HelloWorldStringConcat {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, " + args[0]);
+	}
+
+}
