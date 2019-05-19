c12f7293d520c9a2dacf5a377fee6700e4c5db0e
Johnny Lim <izeye@naver.com>
2019-05-19 17:47:33 +0900

    Add a test for HttpURLConnection.getOutputStream()

--- src/test/java/learningtest/java/net/HttpURLConnectionTests.java
+++ src/test/java/learningtest/java/net/HttpURLConnectionTests.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.ServerSocket;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
@@ -37,6 +38,27 @@ public class HttpURLConnectionTests {
 		assertThat(connection.getResponseCode()).isEqualTo(HttpURLConnection.HTTP_OK);
 		connection.disconnect();
 		assertThat(connection.getResponseCode()).isEqualTo(HttpURLConnection.HTTP_OK);
+	}
+
+	@Test
+	public void getOutputStream() throws IOException {
+		new Thread(() -> {
+			try (ServerSocket serverSocket = new ServerSocket(8080)) {
+				serverSocket.accept();
+			}
+			catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		}).start();
+
+		HttpURLConnection connection = (HttpURLConnection) URI.create("http://localhost:8080").toURL()
+				.openConnection();
+
+		assertThat(connection.getRequestMethod()).isEqualTo("GET");
+		connection.setDoOutput(true);
+		connection.getOutputStream();
+		assertThat(connection.getRequestMethod()).isEqualTo("POST");
+
 	}
 
 }
