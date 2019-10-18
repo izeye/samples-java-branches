a3ecdc7d13479ba797857ac9d8f439611d971beb
Johnny Lim <izeye@naver.com>
2019-10-18 19:29:30 +0900

    Experiment Javadoc @value

---
+++ src/main/java/com/izeye/SomeConstants.java
@@ -0,0 +1,18 @@
+package com.izeye;
+
+/**
+ * Some constants.
+ *
+ * @author Johnny Lim
+ */
+public final class SomeConstants {
+
+	/**
+	 * For some constant 1: {@value}
+	 */
+	public static final String SOME_CONSTANT_1 = "Hello, world!";
+
+	private SomeConstants() {
+	}
+
+}
