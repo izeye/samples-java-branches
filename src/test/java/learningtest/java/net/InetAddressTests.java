68037cbb3bb10762a864fec770d0f75a2c654797
Johnny Lim <izeye@naver.com>
2020-01-22 13:53:55 +0900

    Add a test for InetAddress.isSiteLocalAddress()

--- src/test/java/learningtest/java/net/InetAddressTests.java
+++ src/test/java/learningtest/java/net/InetAddressTests.java
@@ -5,6 +5,8 @@ import java.net.UnknownHostException;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Tests for {@link InetAddress}.
  *
@@ -13,7 +15,7 @@ import org.junit.Test;
 public class InetAddressTests {
 
 	@Test
-	public void testGetLocalHost() throws UnknownHostException {
+	public void getHostAddress() throws UnknownHostException {
 		long startTimestampInMillis = System.currentTimeMillis();
 		InetAddress localHost = InetAddress.getLocalHost();
 		long elapsedTimeInMillis = System.currentTimeMillis() - startTimestampInMillis;
@@ -27,11 +29,21 @@ public class InetAddressTests {
 	}
 
 	@Test
-	public void testGetAllByName() throws UnknownHostException {
+	public void getAllByName() throws UnknownHostException {
 		InetAddress[] addresses = InetAddress.getAllByName("naver.com");
 		for (InetAddress address : addresses) {
 			System.out.println(address.getHostAddress());
 		}
+	}
+
+	@Test
+	public void isSiteLocalAddress() throws UnknownHostException {
+		assertThat(InetAddress.getByName("10.1.2.3").isSiteLocalAddress()).isTrue();
+		assertThat(InetAddress.getByName("11.1.2.3").isSiteLocalAddress()).isFalse();
+		assertThat(InetAddress.getByName("172.16.1.2").isSiteLocalAddress()).isTrue();
+		assertThat(InetAddress.getByName("173.16.1.2").isSiteLocalAddress()).isFalse();
+		assertThat(InetAddress.getByName("192.168.1.2").isSiteLocalAddress()).isTrue();
+		assertThat(InetAddress.getByName("193.168.1.2").isSiteLocalAddress()).isFalse();
 	}
 
 }
