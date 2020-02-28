2d30ec2f0a32a904e6edab83c67f31c7a922d317
Johnny Lim <izeye@naver.com>
2020-02-28 12:54:01 +0900

    Allow JUnit Jupiter tests

---
+++ src/test/java/learningtest/org/junit/jupiter/JUnitJupiterTests.java
@@ -0,0 +1,17 @@
+package learningtest.org.junit.jupiter;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for JUnit Jupiter.
+ *
+ * @author Johnny Lim
+ */
+class JUnitJupiterTests {
+
+	@Test
+	void test() {
+		System.out.println("Hello, JUnit Jupiter!");
+	}
+
+}
