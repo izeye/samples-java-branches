4c5e17bfe9663caf40618b2a1d119fc63de9f6a2
izeye <izeye@naver.com>
2020-04-27 15:42:00 +0900

    Polish ObjectMapperNumberTests

--- src/test/java/learningtest/com/fasterxml/jackson/databind/ObjectMapperNumberTests.java
+++ src/test/java/learningtest/com/fasterxml/jackson/databind/ObjectMapperNumberTests.java
@@ -3,15 +3,14 @@ package learningtest.com.fasterxml.jackson.databind;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Tests for {@link ObjectMapper} with numbers.
@@ -19,9 +18,6 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Johnny Lim
  */
 public class ObjectMapperNumberTests {
-
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
 	public void test() throws IOException {
@@ -31,13 +27,9 @@ public class ObjectMapperNumberTests {
 		List<Long> favoriteNumbers = (List<Long>) map.get("favoriteNumbers");
 
 		assertThat(favoriteNumbers.get(2)).isInstanceOf(Long.class);
-
-		this.thrown.expect(ClassCastException.class);
-		this.thrown.expectMessage("java.lang.Integer cannot be cast to java.lang.Long");
-		Long favoriteNumber = favoriteNumbers.get(0);
-
-		// Unreachable.
-		System.out.println(favoriteNumber);
+		assertThatThrownBy(() -> favoriteNumbers.get(0))
+				.isExactlyInstanceOf(ClassCastException.class)
+				.hasMessageContaining("java.lang.Integer cannot be cast to java.lang.Long");
 	}
 
 	@Test
