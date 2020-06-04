358045ec7ce2e20c1d0ba8a561342c8651342958
Johnny Lim <izeye@naver.com>
2020-06-04 18:25:45 +0900

    Replace deprecated Matchers reference

--- src/test/java/learningtest/org/mockito/MockitoTests.java
+++ src/test/java/learningtest/org/mockito/MockitoTests.java
@@ -8,7 +8,7 @@ import org.mockito.exceptions.misusing.UnfinishedStubbingException;
 import org.mockito.stubbing.OngoingStubbing;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
