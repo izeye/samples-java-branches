ba161500fa0470e1bb1c4fed9c7d5ad9508bd028
Johnny Lim <izeye@naver.com>
2020-06-14 19:44:17 +0900

    Add a test for Arrays.asList()

--- src/test/java/learningtest/java/util/ArraysTests.java
+++ src/test/java/learningtest/java/util/ArraysTests.java
@@ -1,6 +1,7 @@
 package learningtest.java.util;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,11 +15,21 @@ class ArraysTests {
 
     @Test
     void testToString() {
-        Object[] objects = {1, "Hello, world!"};
+        Object[] objects = { 1, "Hello, world!" };
         System.out.println(objects);
 
         String prettyString = Arrays.toString(objects);
         assertThat(prettyString).isEqualTo("[1, Hello, world!]");
+    }
+
+    @Test
+    void asList() {
+        String[] array = { "a", "b", "c", "d", "e", "f" };
+        List<String> list = Arrays.asList(array);
+        array[0] = "XXX";
+        assertThat(list).containsExactly("XXX", "b", "c", "d", "e", "f");
+        list.set(1, "YYY");
+        assertThat(array).containsExactly("XXX", "YYY", "c", "d", "e", "f");
     }
 
 }
