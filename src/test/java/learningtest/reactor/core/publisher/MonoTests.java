2487fe642a9c83b04601b577c28b53ad273f5b00
Johnny Lim <izeye@naver.com>
2020-08-25 14:13:30 +0900

    Add tests for Mono.zip()

--- src/test/java/learningtest/reactor/core/publisher/MonoTests.java
+++ src/test/java/learningtest/reactor/core/publisher/MonoTests.java
@@ -3,9 +3,12 @@ package learningtest.reactor.core.publisher;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,6 +47,53 @@ class MonoTests {
 		assertThat(mono2.block()).isEqualTo(set2);
 		long elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis;
 		assertThat(TimeUnit.MILLISECONDS.toSeconds(elapsedTimeMillis)).isGreaterThanOrEqualTo(2);
+	}
+
+	@Test
+	void zip() {
+		Set<String> stringSet1 = new HashSet<>(Arrays.asList("apple"));
+		Set<String> stringSet2 = new HashSet<>(Arrays.asList("banana"));
+		Set<String> stringAll = new HashSet<>();
+		stringAll.addAll(stringSet1);
+		stringAll.addAll(stringSet2);
+
+		Set<Integer> integerSet1 = new HashSet<>(Arrays.asList(1));
+		Set<Integer> integerSet2 = new HashSet<>(Arrays.asList(2));
+		Set<Integer> integerAll = new HashSet<>();
+		integerAll.addAll(integerSet1);
+		integerAll.addAll(integerSet2);
+
+		Mono<Set<String>> stringMono1 = Mono.just(stringSet1).delayElement(Duration.ofSeconds(1));
+		Mono<Set<String>> stringMono2 = Mono.just(stringSet2).delayElement(Duration.ofSeconds(1));
+		Mono<Set<Integer>> integerMono1 = Mono.just(integerSet1).delayElement(Duration.ofSeconds(1));
+		Mono<Set<Integer>> integerMono2 = Mono.just(integerSet2).delayElement(Duration.ofSeconds(1));
+
+		long startTimeMillis = System.currentTimeMillis();
+
+		Flux<String> mergedStringFlux = Flux.merge(
+				stringMono1.flatMapMany(Flux::fromIterable), stringMono2.flatMapMany(Flux::fromIterable));
+		Mono<List<String>> listMono1 = mergedStringFlux.collectList();
+
+		Flux<Integer> mergedIntegerFlux = Flux.merge(
+				integerMono1.flatMapMany(Flux::fromIterable), integerMono2.flatMapMany(Flux::fromIterable));
+		Mono<List<Integer>> listMono2 = mergedIntegerFlux.collectList();
+
+		Mono<Zipped> zippedMono = Mono.zip(
+				listMono1, listMono2, (mono1, mono2) -> new Zipped(new HashSet<>(mono1), new HashSet<>(mono2)));
+		Zipped zipped = zippedMono.block();
+		assertThat(zipped.getStringSet()).isEqualTo(stringAll);
+		assertThat(zipped.getIntegerSet()).isEqualTo(integerAll);
+
+		long elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis;
+		assertThat(TimeUnit.MILLISECONDS.toSeconds(elapsedTimeMillis)).isLessThan(2);
+	}
+
+	@Data
+	private static class Zipped {
+
+		private final Set<String> stringSet;
+		private final Set<Integer> integerSet;
+
 	}
 
 }
