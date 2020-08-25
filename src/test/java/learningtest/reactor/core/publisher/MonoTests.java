b77f4990684fe02af4dfc1bef22bf24482f4b4dc
Johnny Lim <izeye@naver.com>
2020-08-25 13:12:23 +0900

    Add more tests for Mono.delayElement()

--- src/test/java/learningtest/reactor/core/publisher/MonoTests.java
+++ src/test/java/learningtest/reactor/core/publisher/MonoTests.java
@@ -1,6 +1,9 @@
 package learningtest.reactor.core.publisher;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
@@ -28,6 +31,19 @@ class MonoTests {
 		assertThat(mono.block()).isEqualTo(string);
 		long elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis;
 		assertThat(TimeUnit.MILLISECONDS.toSeconds(elapsedTimeMillis)).isGreaterThanOrEqualTo(1);
+	}
+
+	@Test
+	void delayElementWithMultipleMonos() {
+		Set<String> set1 = new HashSet<>(Arrays.asList("apple"));
+		Set<String> set2 = new HashSet<>(Arrays.asList("banana"));
+		Mono<Set<String>> mono1 = Mono.just(set1).delayElement(Duration.ofSeconds(1));
+		Mono<Set<String>> mono2 = Mono.just(set2).delayElement(Duration.ofSeconds(1));
+		long startTimeMillis = System.currentTimeMillis();
+		assertThat(mono1.block()).isEqualTo(set1);
+		assertThat(mono2.block()).isEqualTo(set2);
+		long elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis;
+		assertThat(TimeUnit.MILLISECONDS.toSeconds(elapsedTimeMillis)).isGreaterThanOrEqualTo(2);
 	}
 
 }
