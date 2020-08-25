66c6afd6d92459baf821fea3bd582f24e5806945
Johnny Lim <izeye@naver.com>
2020-08-25 13:08:43 +0900

    Add tests for Mono.delayElement()

--- src/test/java/learningtest/reactor/core/publisher/MonoTests.java
+++ src/test/java/learningtest/reactor/core/publisher/MonoTests.java
@@ -1,5 +1,7 @@
 package learningtest.reactor.core.publisher;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,6 +18,16 @@ class MonoTests {
 		String string = "Hello, world!";
 		Mono<Integer> length = Mono.just(string).map((s) -> s.length());
 		assertThat(length.block()).isEqualTo(string.length());
+	}
+
+	@Test
+	void delayElement() {
+		String string = "Hello";
+		Mono<String> mono = Mono.just(string).delayElement(Duration.ofSeconds(1));
+		long startTimeMillis = System.currentTimeMillis();
+		assertThat(mono.block()).isEqualTo(string);
+		long elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis;
+		assertThat(TimeUnit.MILLISECONDS.toSeconds(elapsedTimeMillis)).isGreaterThanOrEqualTo(1);
 	}
 
 }
