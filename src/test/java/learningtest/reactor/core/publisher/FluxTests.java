2fbf78056c09bc2cc35ded7fa5c68c7cd3fbada2
Johnny Lim <izeye@naver.com>
2020-10-27 17:19:50 +0900

    Add tests for Flux.subscribe() with subscriber

--- src/test/java/learningtest/reactor/core/publisher/FluxTests.java
+++ src/test/java/learningtest/reactor/core/publisher/FluxTests.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -86,6 +88,29 @@ class FluxTests {
 					System.out.println("Subscription: " + subscription);
 					subscription.request(10);
 				});
+	}
+
+	@Test
+	void subscribeWithSubscriber() {
+		SampleSubscriber<Integer> sampleSubscriber = new SampleSubscriber<>();
+		Flux<Integer> integers = Flux.range(1, 4);
+		integers.subscribe(sampleSubscriber);
+	}
+
+	private static class SampleSubscriber<T> extends BaseSubscriber<T> {
+
+		@Override
+		protected void hookOnSubscribe(Subscription subscription) {
+			System.out.println("Subscribed");
+			request(1);
+		}
+
+		@Override
+		protected void hookOnNext(T value) {
+			System.out.println(value);
+			request(1);
+		}
+
 	}
 
 	@Test
