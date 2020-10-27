0f02a09605bbe2fb291e10f959c9a200316d22ab
Johnny Lim <izeye@naver.com>
2020-10-27 13:06:23 +0900

    Add tests for NumberFormat with BigDecimal

--- src/test/java/learningtest/java/text/NumberFormatTests.java
+++ src/test/java/learningtest/java/text/NumberFormatTests.java
@@ -2,6 +2,7 @@ package learningtest.java.text;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.Locale;
 
@@ -20,6 +21,13 @@ class NumberFormatTests {
         assertThat(NumberFormat.getInstance(Locale.JAPANESE).format(1000)).isEqualTo("1,000");
         assertThat(NumberFormat.getInstance(Locale.US).format(1000)).isEqualTo("1,000");
         assertThat(NumberFormat.getInstance(Locale.FRENCH).format(1000)).isEqualTo("1 000");
+    }
+
+    @Test
+    void formatWithBigDecimal() {
+        BigDecimal decimal = BigDecimal.valueOf(1001001000);
+        BigDecimal divided = decimal.divide(BigDecimal.valueOf(1_000_000));
+        assertThat(NumberFormat.getInstance(Locale.KOREAN).format(divided)).isEqualTo("1,001.001");
     }
 
 }
