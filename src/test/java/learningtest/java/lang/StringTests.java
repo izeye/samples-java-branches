67d76242557f5a123b742b4f82e96c5ca966084f
izeye <izeye@naver.com>
2021-01-20 12:41:10 +0900

    Migrate StringTests to JUnit Jupiter

--- src/test/java/learningtest/java/lang/StringTests.java
+++ src/test/java/learningtest/java/lang/StringTests.java
@@ -1,6 +1,6 @@
 package learningtest.java.lang;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,10 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Johnny Lim
  */
-public class StringTests {
+class StringTests {
 	
 	@Test
-	public void format() {
+	void format() {
 		Integer i = 7;
 		assertThat(String.format("%d", i)).isEqualTo("7");
 		assertThat(String.format("%s", i)).isEqualTo("7");
@@ -25,48 +25,48 @@ public class StringTests {
 	}
 
 	@Test
-	public void valueOfBoolean() {
+	void valueOfBoolean() {
 		assertThat(String.valueOf(true)).isEqualTo("true");
 		assertThat(String.valueOf(false)).isEqualTo("false");
 	}
 
 	@Test
-	public void replaceAll() {
+	void replaceAll() {
 		assertThat("a:b:c".replaceAll(":", "")).isEqualTo("abc");
 	}
 
 	@Test
-	public void replaceAllWithLineSeparators() {
+	void replaceAllWithLineSeparators() {
 		assertThat("a\rb\nc\r\nd".replaceAll("[\r\n]+", "")).isEqualTo("abcd");
 	}
 
 	@Test
-	public void replace() {
+	void replace() {
 		assertThat("a+b+c".replace("+", "%20")).isEqualTo("a%20b%20c");
 		assertThat("a.b.c".replace(".", "_")).isEqualTo("a_b_c");
 	}
 
 	@Test
-	public void replaceFirst() {
+	void replaceFirst() {
 		String regex = "^https?://[^/]+/";
 		assertThat("https://www.google.com/hello".replaceFirst(regex, "")).isEqualTo("hello");
 		assertThat("/hello".replaceFirst(regex, "")).isEqualTo("/hello");
 	}
 
 	@Test
-	public void substring() {
+	void substring() {
 		assertThat("Hello, world!".substring(0, 0)).isEmpty();
 	}
 
 	@Test
-	public void split() {
+	void split() {
 		assertThat(" ".split(",")).containsExactly(" ");
 		assertThat(",".split(",")).isEmpty();
 		assertThat(", ".split(",")).containsExactly("", " ");
 	}
 
 	@Test
-	public void splitWithLimit() {
+	void splitWithLimit() {
 		assertThat("".split(":", 2)).containsExactly("");
 		assertThat(":".split(":", 2)).containsExactly("", "");
 		assertThat("a:".split(":", 2)).containsExactly("a", "");
