998de0e2d73090df5be1c0f2b33735b27fe8cbf7
izeye <izeye@naver.com>
2021-08-20 16:18:30 +0900

    Add a test for Document.select() with attribute

--- src/test/java/learningtest/org/jsoup/JsoupTests.java
+++ src/test/java/learningtest/org/jsoup/JsoupTests.java
@@ -2,6 +2,7 @@ package learningtest.org.jsoup;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
@@ -15,13 +16,27 @@ import java.io.IOException;
 class JsoupTests {
 	
 	@Test
-	void test() {
+	void connect() {
 		try {
 			Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
 			Elements newsHeadlines = doc.select("#mp-itn b a");
 			System.out.println(newsHeadlines);
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
+		}
+	}
+
+	@Test
+	void selectWithAttribute() {
+		String html = "<head>"
+				+ "<link rel=\"stylesheet\" href=\"https://www.izeye.com/test1.css\">"
+				+ "<link rel=\"stylesheet\" href=\"https://www.izeye.com/test2.css\">"
+				+ "</head/>";
+		Document document = Jsoup.parse(html);
+		Elements elements = document.select("[href]");
+		for (Element element : elements) {
+			String href = element.attr("href");
+			System.out.println(href);
 		}
 	}
 	
