c0e138838c4ba67aca618d7d095304eb9571a214
izeye <izeye@naver.com>
2021-11-23 21:59:29 +0900

    Add a test for Iterator.remove()

--- src/test/java/learningtest/java/util/IteratorTests.java
+++ src/test/java/learningtest/java/util/IteratorTests.java
@@ -1,25 +1,28 @@
 package learningtest.java.util;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ConcurrentModificationException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Tests for {@link Iterator}.
  *
  * @author Jungmuk Lim
  */
-public class IteratorTests {
+class IteratorTests {
 
 	@Test
-	public void testForEachRemaining() {
+	void testForEachRemaining() {
 		Iterator<String> iterator = Arrays.asList("Johnny", "John").iterator();
 
 		List<String> names = new ArrayList<>();
@@ -29,7 +32,7 @@ public class IteratorTests {
 	}
 
 	@Test
-	public void nextWhenRemovingWithIteratorShouldWork() {
+	void nextWhenRemovingWithIteratorShouldWork() {
 		List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
 		Iterator<Integer> iterator = list.iterator();
 		assertThat(iterator.next()).isEqualTo(1);
@@ -40,7 +43,7 @@ public class IteratorTests {
 	}
 
 	@Test
-	public void nextWhenRemovingWithListWillThrowConcurrentModificationException() {
+	void nextWhenRemovingWithListWillThrowConcurrentModificationException() {
 		List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
 		Iterator<Integer> iterator = list.iterator();
 		Integer element = iterator.next();
@@ -50,6 +53,20 @@ public class IteratorTests {
 		assertThat(list).hasSize(3);
 		assertThatExceptionOfType(ConcurrentModificationException.class)
 				.isThrownBy(() -> iterator.next());
+	}
+
+	@Test
+	void removeWithoutNextThrowsIllegalStateException() {
+		Map<String, String> map = new HashMap<>();
+		map.put("1", "a");
+		map.put("2", "b");
+		map.put("3", "c");
+
+		Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+		assertThatThrownBy(iterator::remove).isInstanceOf(IllegalStateException.class);
+		iterator.next();
+		iterator.remove();
+		assertThatThrownBy(iterator::remove).isInstanceOf(IllegalStateException.class);
 	}
 
 }
