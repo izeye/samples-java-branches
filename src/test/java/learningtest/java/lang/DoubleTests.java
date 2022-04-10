fe7f29085987b9342cda59ae19728fe9bb600c90
izeye <izeye@naver.com>
2022-04-10 23:15:03 +0900

    Test equality operator against Double objects

--- src/test/java/learningtest/java/lang/DoubleTests.java
+++ src/test/java/learningtest/java/lang/DoubleTests.java
@@ -17,4 +17,18 @@ class DoubleTests {
 		assertThat(value).isNaN();
 	}
 
+	@Test
+	void equalityOperatorWithObjectsFromValueOf() {
+		assertThat(Double.valueOf(1) == Double.valueOf(1)).isFalse();
+		assertThat(Double.valueOf(Double.MAX_VALUE) == Double.MAX_VALUE).isTrue();
+		assertThat(Double.valueOf(Double.POSITIVE_INFINITY) == Double.POSITIVE_INFINITY).isTrue();
+	}
+
+	@Test
+	void equalityOperatorWithObjectsFromConstructor() {
+		assertThat(new Double(1) == new Double(1)).isFalse();
+		assertThat(new Double(Double.MAX_VALUE) == Double.MAX_VALUE).isTrue();
+		assertThat(new Double(Double.POSITIVE_INFINITY) == Double.POSITIVE_INFINITY).isTrue();
+	}
+
 }
